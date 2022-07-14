c42bca46c7cec0e16fe71ca5417497b607b78f40
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-14 17:48:22 +0200

    Esercizio Razor 4

--- Controllers/EserciziRazor.cs
+++ Controllers/EserciziRazor.cs
@@ -19,24 +19,38 @@ namespace la_mia_pizzeria_static.Controllers
                 }
                 return sum;
             }
-
             ViewData["SumResult"] = Sum(10);
 
-            int SumA()
+            int CountA()
             {
-                //string str = "Techie Delight";
-                //char ch = 'e';
-                //int freq = str.Count(f => (f == ch));
-                //Console.WriteLine(freq);
-
                 string text = "nel mezzo del cammin di nostra vita";
                 char character = 'a';
                 int freq = text.Count(f => (f == character));
 
                 return freq;
             }
+            ViewData["HowManyA"] = CountA();
 
-            ViewData["HowManyA"] = SumA();
+            List<int> nums = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                int num = new Random().Next(1, 91);
+                nums.Add(num);
+            }
+
+            List<int> validNums = new List<int>();
+            foreach (int num in nums)
+            {
+                if (num>=20 && num<=70)
+                {
+                    validNums.Add(num);
+                }
+            }  
+            
+
+            string combinedString = string.Join(",", validNums.ToArray());
+            ViewData["RndNum"] = combinedString;
+
             return View();
         }
 
@@ -55,21 +69,21 @@ namespace la_mia_pizzeria_static.Controllers
 //3. Contare quante volte la lettera a è presente nalla frase "nel mezzo del cammin di nostra vita" e stampare questo valore in pagina
 
 
-
-
-
-
-
-
-
-
 //4. Creare una List<int> di 20 numeri interi (generati random da 1 a 90, 1 e 90 inclusi) e mostrare in pagina solo i numeri compresi tra 20 e 70
+
+
+
+
+
+
+
+
 //5. Stampare in pagina la data e l'ora attuale (usando la classe DateTime) con il formato anno-mese-giorno ore:minuti:secondi
 //6. Partendo dalla data di oggi (usando la classe DateTime) stampare in pagina
 //a.la data di 7 giorni fa
 //b.che giorno della settimana è oggi (il risultato dovrà essere il nome del giorno (Lunedì, Martedì, ...)
 //c.che giorno della settimana sarà nello stesso giorno e mese di oggi ma tra un anno (il risultato dovrà essere il nome del giorno (Lunedì, Martedì, ...)
-//Riduci
+
 
 
 
