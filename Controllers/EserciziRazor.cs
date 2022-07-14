01751c89c55120cac7ff296246e3501746a47034
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-14 15:29:08 +0200

    Esercizio Razor 2

--- Controllers/EserciziRazor.cs
+++ Controllers/EserciziRazor.cs
@@ -9,16 +9,43 @@ namespace la_mia_pizzeria_static.Controllers
             string name = "Laura";
             string surname = "Margherita";
             ViewData["Message"] = name + " " + surname;
+
+            int Sum(int maxNum)
+            {
+                int sum = 0;
+                for (int i = 0; i <= maxNum; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+
+            ViewData["SumResult"] = Sum(10);
             return View();
         }
+
     }
 }
 
 //1.Dichiarare una variabile valorizzandola con il nostro nome.
 //Dichiarare un'altra variabile valorizzandola con il nostro cognome.
 //Stampare il nostro nome e cognome all'interno di un tag h2.
+
+
 //2. Creare una funzione che stampa la somma dei numeri che vanno da 1 a n (con n passato come parametro - n incluso)
 //Stampare dentro uno span il risultato.
+
+
+
+
+
+
+
+
+
+
+
+
 //3. Contare quante volte la lettera a è presente nalla frase "nel mezzo del cammin di nostra vita" e stampare questo valore in pagina
 //4. Creare una List<int> di 20 numeri interi (generati random da 1 a 90, 1 e 90 inclusi) e mostrare in pagina solo i numeri compresi tra 20 e 70
 //5. Stampare in pagina la data e l'ora attuale (usando la classe DateTime) con il formato anno-mese-giorno ore:minuti:secondi
